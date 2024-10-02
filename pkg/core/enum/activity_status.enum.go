376a5edc44e98b010796da12543f56b79ff93879
amirphl <amirphl4@gmail.com>
2024-10-02 11:44:06 +0000

    feat(enum): Add ActivityStatus enum with scan and conversion functions
    - Introduced `ActivityStatus` enum to represent various activity states: `none`, `enabled`, `disabled`, `suspend`, `locked`, `activated`, and `inactivated`.
    - Implemented the `Scan` method to handle parsing of `ActivityStatus` from string or `[]byte` values, returning an error for invalid inputs.
    - Added `String` method for easy string representation of `ActivityStatus`.
    - Introduced `AstatsFromStrings` function to convert a list of strings into a slice of `ActivityStatus` values, with error handling for invalid string entries.
    
    This commit adds a structured approach to manage activity statuses, including functionality for parsing and converting from strings.

---
+++ pkg/core/enum/activity_status.enum.go
@@ -0,0 +1,69 @@
+package enum
+
+import (
+	"errors"
+)
+
+type ActivityStatus string
+
+// Write a function to sort a list of integers
+const (
+	ActivityStatusNone        ActivityStatus = "none"
+	ActivityStatusEnabled     ActivityStatus = "enabled"
+	ActivityStatusDisabled    ActivityStatus = "disabled"
+	ActivityStatusSuspend     ActivityStatus = "suspend"
+	ActivityStatusLocked      ActivityStatus = "locked"
+	ActivityStatusActivated   ActivityStatus = "activated"
+	ActivityStatusInactivated ActivityStatus = "inactivated"
+)
+
+func (e *ActivityStatus) Scan(value interface{}) error {
+	var enumValue string
+	switch val := value.(type) {
+	case string:
+		enumValue = val
+	case []byte:
+		enumValue = string(val)
+	default:
+		return errors.New("invalid scan value for AllTypesEnum enum. Enum value has to be of type string or []byte")
+	}
+
+	switch enumValue {
+	case "none":
+		*e = ActivityStatusNone
+	case "enabled":
+		*e = ActivityStatusEnabled
+	case "disabled":
+		*e = ActivityStatusDisabled
+	case "suspend":
+		*e = ActivityStatusSuspend
+	case "locked":
+		*e = ActivityStatusLocked
+	case "activated":
+		*e = ActivityStatusActivated
+	case "inactivated":
+		*e = ActivityStatusInactivated
+	default:
+		return errors.New("jet: Invalid scan value '" + enumValue + "' for ActivityStatus enum")
+	}
+
+	return nil
+}
+
+func (e ActivityStatus) String() string {
+	return string(e)
+}
+
+func AstatsFromStrings(in []string) ([]ActivityStatus, error) {
+	out := make([]ActivityStatus, 0, len(in))
+
+	for _, v := range in {
+		var astat ActivityStatus
+		if err := astat.Scan(v); err != nil {
+			return nil, err
+		}
+		out = append(out, astat)
+	}
+
+	return out, nil
+}
